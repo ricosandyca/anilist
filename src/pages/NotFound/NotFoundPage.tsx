11120cefef3b280468e2c804c955a888198c0efd
Rico Sandyca <ricosandyca.if@gmail.com>
2022-03-20 05:34:59 +0700

    Design not found page

--- src/pages/NotFound/NotFoundPage.tsx
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -1,7 +1,50 @@
 import { FC } from 'react';
+import {
+  Flex,
+  Text,
+  VStack,
+  HStack,
+  Icon,
+  useBreakpointValue,
+} from '@chakra-ui/react';
+import { RiEmotionSadFill } from 'react-icons/ri';
 
 const NotFoundPage: FC = () => {
-  return <div>page not found</div>;
+  // responsive breakpoints
+  const isSMDown = useBreakpointValue({ base: true, md: false });
+  const Wrapper = isSMDown ? VStack : HStack;
+
+  return (
+    <Flex
+      position="relative"
+      w="full"
+      h={`calc(100vh - 300px)`}
+      alignItems="center"
+      justifyContent="center"
+      color="whiteAlpha.800"
+    >
+      <Text
+        position="absolute"
+        zIndex={-1}
+        fontWeight="extrabold"
+        fontSize={['10rem', '18rem']}
+        userSelect="none"
+        w="full"
+        textAlign="center"
+        opacity={0.04}
+      >
+        404
+      </Text>
+      <Wrapper spacing={3} alignItems="center">
+        <Icon as={RiEmotionSadFill} fontSize="6xl" color="inherit" />
+        <VStack alignItems={!isSMDown ? 'flex-start' : 'center'} spacing={1}>
+          <Text color="inherit" fontWeight="semibold">
+            Page Not Found
+          </Text>
+        </VStack>
+      </Wrapper>
+    </Flex>
+  );
 };
 
 export default NotFoundPage;
