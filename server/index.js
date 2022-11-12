64a024cb11611f3622d97222b950edf51cee90be
Rico Sandyca Novenza <ricosandyca.if@gmail.com>
2022-11-12 21:14:52 +0700

    Add anilist graphql proxy

---
+++ server/index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+const port = process.env.PORT;
+const apiTarget = process.env.API_TARGET;
+
+// development purpose only
+if (process.env.NODE_ENV === 'development') app.use(cors());
+
+app.use(
+  '/api',
+  createProxyMiddleware({
+    target: apiTarget,
+    changeOrigin: true,
+  })
+);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
